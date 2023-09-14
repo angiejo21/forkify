c33098c9160aa84d6b62f163f294be5e68065b65
Angela Bellò <angela.bello90@gmail.com>
2023-09-14 16:48:57 +0200

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -113,6 +113,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application!');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -121,5 +125,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 init();
